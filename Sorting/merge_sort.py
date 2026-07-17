2d6621b0c9772eb349851b37952767a25865ecd6
ADITYA KUMAR ROY <adityakumarroy401@gmail.com>
2026-07-17 19:17:46 +0530

    Add: Merge Sort using Divide and Conquer (O(n log n))

---
+++ Sorting/merge_sort.py
@@ -0,0 +1,35 @@
+class Solution:
+    def merge(self, arr, low, mid, high):
+        temp = []
+        left = low
+        right = mid + 1
+
+        while left <= mid and right <= high:
+            if arr[left] <= arr[right]:
+                temp.append(arr[left])
+                left += 1
+            else:
+                temp.append(arr[right])
+                right += 1
+
+        while left <= mid:
+            temp.append(arr[left])
+            left += 1
+
+        while right <= high:
+            temp.append(arr[right])
+            right += 1
+
+        for i in range(low, high + 1):
+            arr[i] = temp[i - low]
+
+    def mergeSort(self, arr, low, high):
+        if low >= high:
+            return
+
+        mid = (low + high) // 2
+
+        self.mergeSort(arr, low, mid)
+        self.mergeSort(arr, mid + 1, high)
+
+        self.merge(arr, low, mid, high)
